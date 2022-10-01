787916176e24c54e308a271d97d8e18c414c4849
xiaohua <3356909177@qq.com>
2022-10-01 22:56:51 +0800

    列表初始化

---
+++ src/components/MyList.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="todo-main">
+    <MyItem v-for="todoObj in todos" :key="todoObj.id" :todo="todoObj" />
+  </ul>
+</template>
+
+<script>
+import MyItem from "./MyItem";
+
+export default {
+  name: "MyList",
+  components: { MyItem },
+  data() {
+    return {
+      todos: [
+        { id: "001", title: "吃饭", done: true },
+        { id: "002", title: "睡觉", done: false },
+        { id: "003", title: "打代码", done: true },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
